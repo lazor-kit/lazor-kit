1efecdc391361c1357504e602c7b112552f10397
Kay( Andrew ) <chauanhtuan185@gmail.com>
2025-06-02 20:50:20 +0700

    feat: remove old module - add types

--- src/core/delay.ts
+++
@@ -1,5 +0,0 @@
-// /src/wallet/modules/delay.ts
-export const delay = (seconds: number): Promise<void> => {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-  };
-  
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// Export UI Components
-export * from './ui';
-
-// Export Hooks
-export * from './hook';
-
