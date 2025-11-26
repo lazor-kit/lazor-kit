d2773d0749f0a320097c255b9b687343c75c07b1
Kay ( Andrew ) <chauanhtuan185@gmail.com>
2025-11-26 20:35:49 +0700

    chore: remove old program folder after moving to submodule

--- packages/program/programs/lazorkit/src/instructions/mod.rs
+++
@@ -1,7 +0,0 @@
-mod create_smart_wallet;
-mod execute_instruction;
-mod initialize;
-
-pub use create_smart_wallet::*;
-pub use execute_instruction::*;
-pub use initialize::*;
--- packages/program/programs/lazorkit/src/state/message.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[derive(Default, AnchorSerialize, AnchorDeserialize, Debug)]
-pub struct Message {
-    pub nonce: u64,
-    pub current_slot: i64,
-    pub instruction_data: Vec<u8>,
-}
